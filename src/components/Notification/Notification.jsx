ade17db91fd0035597008e60bfa1ab983937ca86
wcAvi <wc.avi1997@gmail.com>
2025-05-06 10:45:25 +0530

    updated api services and api changes.

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 const Notification = () => {
+    useEffect(() => {
+        // Ensure the Quickhunt_Config array exists
+        window.Quickhunt_Config = window.Quickhunt_Config || [];
+        // Push the configuration object
+        window.Quickhunt_Config.push({
+            Quickhunt_Widget_Key: "NnRwS0JmM1orRVNEbFc2cy9RdjRMZz09Ojozb0lVUU5CbXp3UkhuQjZoMmhKZTVRPT0="
+        });
+
+        // Dynamically load the external script
+        const script = document.createElement('script');
+        script.src = "https://widget.quickhunt.app/widgetScript.js";
+        script.async = true;
+        document.body.appendChild(script);
+
+        // Cleanup script on component unmount
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
     return (
         <div>
-            <iframe src="https://quickhuntapp.quickhunt.app/widget/ideas?widget=311"
-                    style={{border: "0px", outline: "0px", width: "100%", height: "calc(100vh - 56px)"}}
-            />
+            {/*<iframe src="https://quickhuntapp.quickhunt.app/widget/ideas?widget=311"*/}
+            {/*        style={{border: "0px", outline: "0px", width: "100%", height: "calc(100vh - 56px)"}}*/}
+            {/*/>*/}
         </div>
     );
 };
