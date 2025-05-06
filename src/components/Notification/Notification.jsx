61e719b20357d94e164b8b66a8823ff8ac4aa2af
Palak Vaghasiya <wc.palak@gmail.com>
2025-05-06 11:19:48 +0530

    replace new script

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,30 +1,11 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 
 const Notification = () => {
-    useEffect(() => {
-        // Ensure the Quickhunt_Config array exists
-        window.Quickhunt_Config = window.Quickhunt_Config || [];
-        // Push the configuration object
-        window.Quickhunt_Config.push({
-            Quickhunt_Widget_Key: "NnRwS0JmM1orRVNEbFc2cy9RdjRMZz09Ojozb0lVUU5CbXp3UkhuQjZoMmhKZTVRPT0="
-        });
-
-        // Dynamically load the external script
-        const script = document.createElement('script');
-        script.src = "https://widget.quickhunt.app/widgetScript.js";
-        script.async = true;
-        document.body.appendChild(script);
-
-        // Cleanup script on component unmount
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []);
     return (
         <div>
-            {/*<iframe src="https://quickhuntapp.quickhunt.app/widget/ideas?widget=311"*/}
-            {/*        style={{border: "0px", outline: "0px", width: "100%", height: "calc(100vh - 56px)"}}*/}
-            {/*/>*/}
+            <iframe src="https://qh.quickhunt.app/widget/ideas?widget=NnRwS0JmM1orRVNEbFc2cy9RdjRMZz09Ojozb0lVUU5CbXp3UkhuQjZoMmhKZTVRPT0="
+                    style={{border: "0px", outline: "0px", width: "100%", height: "calc(100vh - 56px)"}}
+            />
         </div>
     );
 };
