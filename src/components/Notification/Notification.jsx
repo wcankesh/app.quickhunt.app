6c841e8b331f676acbb7789c0b6268de15ba3101
Palak Vaghasiya <wc.palak@gmail.com>
2025-05-09 10:56:37 +0530

    change qh to help

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Notification = () => {
     return (
         <div>
-            <iframe src="https://qh.quickhunt.app/widget/ideas?widget=NnRwS0JmM1orRVNEbFc2cy9RdjRMZz09Ojozb0lVUU5CbXp3UkhuQjZoMmhKZTVRPT0="
+            <iframe src="https://help.quickhunt.app/widget/ideas?widget=NnRwS0JmM1orRVNEbFc2cy9RdjRMZz09Ojozb0lVUU5CbXp3UkhuQjZoMmhKZTVRPT0="
                     style={{border: "0px", outline: "0px", width: "100%", height: "calc(100vh - 56px)"}}
             />
         </div>
